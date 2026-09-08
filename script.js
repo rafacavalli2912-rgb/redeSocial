a20d68671e33165eea81db414b2e9508e61b63c3
rafacavalli2912-rgb <rafa.cavalli2912@gmail.com>
2026-09-08 09:00:18 -0300

    ok

--- script.js
+++ script.js
@@ -6,8 +6,30 @@ if(!likeBtn) return;
 const likesCountSpan = likeBtn.querySelector(".likes-count");
 const bookmarkBtn = document.querySelector(".bookmark-btn");
 
+let isLiked = false;
+let baseLiked = 0;
 
+if (likesCountSpan){
+    likesCountSpan.textContent = "0";
+}
 
+//números grandes
 
+function formatLikes(num) {
+    if(num >=1000){
+        return (num/1000).toFixed(1)+"K";
+    }
+    return num.toString();
+}
+
+function addLike(){
+    baseLikes++;
+    isLiked = true;
+    likeBtn.classList.add("liked");
+
+    if(likesCountSpan){
+        likesCountSpan.textContent = formatLikes(baseLikes);
+    }
+}
 
 })
